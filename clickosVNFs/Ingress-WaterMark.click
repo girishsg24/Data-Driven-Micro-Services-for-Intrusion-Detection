0b113f997798a195de57b812238cac21de178160
root <root@router-2.girish0-qv34527.cloudlab.emulab.net>
2018-03-22 18:52:47 -0600

    watermarking the payload click

--- clickosVNFs/Ingress-WaterMark.click
+++ clickosVNFs/Ingress-WaterMark.click
@@ -1,3 +1,18 @@
-//FromDevice->Print(MAXLENGTH -1)->SetWaterMark->Print(MAXLENGTH -1)->ToDevice
-FromDevice->Print(MAXLENGTH -1)->ToDevice
-//FromDevice->SetWaterMark->ToDevice
+define($IP 10.10.3.7);                                                                            
+define($MAC 00:00:00:00:30:00);   
+
+source::FromDevice;                                                                                
+sink::ToDevice;       
+
+c::Classifier(12/0806,12/0800 34/08,-);  
+
+source->c;      
+
+c[0]->ARPResponder($IP $MAC)->sink;                      
+//************************Important piece********************                                                  
+c[1]->CheckIPHeader(14)->Print("Before",MAXLENGTH -1)
+ ->Strip(14)->StripIPHeader
+ ->SetWaterMark
+ ->UnstripIPHeader->Unstrip(14)->Print("After",MAXLENGTH -1)->sink;
+//*************************************************************
+c[2]->sink; 
--- clickosVNFs/setWaterMark.cc
+++ clickosVNFs/setWaterMark.cc
@@ -1,24 +1,83 @@
-/*
- * setcrc32.{cc,hh} -- element sets CRC 32 checksum
- * Robert Morris
- *
- * Copyright (c) 1999-2000 Massachusetts Institute of Technology
- *
- * Permission is hereby granted, free of charge, to any person obtaining a
- * copy of this software and associated documentation files (the "Software"),
- * to deal in the Software without restriction, subject to the conditions
- * listed in the Click LICENSE file. These conditions include: you must
- * preserve this copyright notice, and you cannot mention the copyright
- * holders in advertising related to the Software without their permission.
- * The Software is provided WITHOUT ANY WARRANTY, EXPRESS OR IMPLIED. This
- * notice is a summary of the Click LICENSE file; the license in that file is
- * legally binding.
- */
-
 #include <click/config.h>
-//#include <click/crc32.h>
 #include "setWaterMark.hh"
+#include <stdint.h>
+#include <stddef.h> 
+typedef uint16_t u_int16_t; 
+typedef size_t size_type;
+
 CLICK_DECLS
+/*
+std::string string_to_hex(const std::string& input,size_t& len)
+{
+    static const char* const lut = "0123456789ABCDEF";
+
+    std::string output;
+    output.reserve(3 * len);
+    for (size_t i = 0; i < len; ++i)
+    {
+        const unsigned char c = input[i];
+        output.push_back(lut[c >> 4]);
+        output.push_back(lut[c & 15]);
+        output.push_back(' ');
+    }
+    return output;
+}
+*/
+void insertWaterMark(char* pktPtr, int pktLength,u_int16_t position,char waterMark)
+{
+    char* lastExistingByte = pktPtr+pktLength-1;
+    char* spaceByte = pktPtr + pktLength;
+    printf("\nwatermark: %x", waterMark);
+    printf("\nposition: %x",(int)position);
+    printf("\npacketlength: %i",(int)pktLength);
+    //start with pktlength because of 1 byte increase
+    for (int i=pktLength;i>=0;i--)
+    {
+        if (i == position)
+        {
+            *spaceByte = waterMark;
+            pktLength++;
+            return;
+        }
+        else
+        {
+            *spaceByte = *lastExistingByte;
+        }
+        lastExistingByte--;
+        spaceByte--;
+    }
+    pktLength++;
+}
+
+void encode(char* pktPtr, int pktLen, char* waterMark)
+{
+    char waterMark1 = waterMark[0];
+    char waterMark2 = waterMark[1];
+    char waterMark3 = waterMark[2];
+    char waterMark4 = waterMark[3];
+    u_int16_t pos1 = (3);
+    u_int16_t pos2 = (5);
+    u_int16_t pos3 = (7);
+    u_int16_t pos4 = (9);
+    
+    //cout<<"**********************************************"<<endl;
+    insertWaterMark(pktPtr,pktLen,pos1,waterMark1);
+  // std::string pktHex = string_to_hex(string((const char*)pktPtr),pktLen);
+    //cout<<"Inserting watermark at pos: "<<pos1<<endl<<pktHex<<endl<<endl;
+    
+    insertWaterMark(pktPtr,pktLen+1,pos2,waterMark2);
+   // pktHex = string_to_hex(string((const char*)pktPtr),pktLen);
+    //cout<<"Inserting watermark at pos: "<<pos2<<endl<<pktHex<<endl<<endl;
+    
+    insertWaterMark(pktPtr,pktLen+2,pos3,waterMark3);
+   // pktHex = string_to_hex(string((const char*)pktPtr),pktLen);
+    //cout<<"Inserting watermark at pos: "<<pos3<<endl<<pktHex<<endl<<endl;
+    
+    insertWaterMark(pktPtr,pktLen+3,pos4,waterMark4);
+   // pktHex = string_to_hex(string((const char*)pktPtr),pktLen);
+    //cout<<"Inserting watermark at pos: "<<pos4<<endl<<pktHex<<endl<<endl;
+    //cout<<"**********************************************"<<endl;
+}
 
 SetWaterMark::SetWaterMark()
 {
@@ -28,11 +87,18 @@ Packet *
 SetWaterMark::simple_action(Packet *p)
 {
   int len = p->length();
-  unsigned int waterMark = 0xfffcfcff;
-  
+ // printf("maclength %i,%x",p->mac_length(),p->mac_length());
+ // int payloadOffset = (p->mac_length())+(p->ip_header_length())+(icmpPingHeaderLength);
+  //size_t payloadLen = len-payloadOffset; 
+  //unsigned int waterMark = 0xfffcfcff;
+  char waterMark[5] = {0x73,0x6a,0x73,0x75};
   WritablePacket *q = p->put(4);
-  memcpy(q->data() + len, &waterMark, 4);
-
+  //printf("payloadoffset %i\n",payloadOffset);
+ // printf("payloadoffset %x\n",payloadOffset);
+ // printf("payloadLen %x,%i\n", payloadLen,payloadLen);
+  encode((char*)q->data(),len, waterMark);
+  
+  //memcpy(q->data() + len, &waterMark, 4);
   return(q);
 }
 
